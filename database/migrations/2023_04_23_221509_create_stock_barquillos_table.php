c9be890ceffb79a38171b7c2400f7e15301a5c68
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-05-06 14:54:19 -0500

    new - creacion de migracion de  stock helados, sotck helados detalle, movimientos, tipo documentos, stock baterias, stock barquillos.

---
+++ database/migrations/2023_04_23_221509_create_stock_barquillos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_barquillos', function (Blueprint $table) {
+            $table->id();
+            $table->string("codigo_movimiento");
+            $table->integer("movimientos_id");
+            $table->integer("tipo_documento_id");
+            $table->integer("cantidad");
+            $table->string("numero_documento")->nullable();
+            $table->date("fecha_movimiento")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_barquillos');
+    }
+};
