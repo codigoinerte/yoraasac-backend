2d36c457b108ef8a43a821bf4ea144939f679f93
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-05-06 14:46:27 -0500

    new - Request/StockBarquillos.php

---
+++ app/Http/Requests/StockBarquillos.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StockBarquillos extends FormRequest
+{
+    public function rules(Request $request)
+    {
+        return [
+            'movimientos_id'=> 'required|integer|exists:movimientos,id',
+            'tipo_documento_id'=> 'required|integer|exists:tipo_documentos,id',
+            'cantidad'=> 'required|integer'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+
+    }
+}
