c8b2c9ef5c3415543a94146e1cf1db54b79b360c
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-06-10 10:28:10 -0500

    creacion de models/NotaHeladeroDetalle

---
+++ app/Models/NotaHeladeroDetalle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NotaHeladeroDetalle extends Model
+{
+    use HasFactory;
+
+    protected $table = "nota_heladero_detalle";    
+}
