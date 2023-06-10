250a96979325afabb173f2c45d503ad6c6904f31
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-06-10 10:26:03 -0500

    se creo NotaHeladeroDetalleController

---
+++ app/Http/Controllers/NotaHeladeroDetalleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NotaHeladeroDetalle;
+use Illuminate\Http\Request;
+
+class NotaHeladeroDetalleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NotaHeladeroDetalle  $notaHeladeroDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NotaHeladeroDetalle $notaHeladeroDetalle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NotaHeladeroDetalle  $notaHeladeroDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NotaHeladeroDetalle $notaHeladeroDetalle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NotaHeladeroDetalle  $notaHeladeroDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NotaHeladeroDetalle $notaHeladeroDetalle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NotaHeladeroDetalle  $notaHeladeroDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NotaHeladeroDetalle $notaHeladeroDetalle)
+    {
+        //
+    }
+}
