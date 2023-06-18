e5fbd7f8a80c26335c9815b0cdc13be648b5577e
FreddyMartinezBabilonia <fredy.martinez@babilonia.io>
2023-06-18 18:16:25 -0500

    se creo el modelo de Factura estado

---
+++ app/Models/FacturaEstado.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FacturaEstado extends Model
+{
+    use HasFactory;
+
+    protected $table="factura_estados";
+}
