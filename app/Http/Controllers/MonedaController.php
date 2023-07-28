e1303b94a12120c8333f0707a9e5fe67b85d43b2
FreddyMartinezBabilonia <fredy.martinez@babilonia.io>
2023-07-27 20:04:32 -0500

    feat: creacion de metodos para actualizar, eliminar y guardar moneda

--- app/Http/Controllers/MonedaController.php
+++ app/Http/Controllers/MonedaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Moneda;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ResponseController;
+use App\Http\Requests\Moneda as RequestMoneda;
 
 class MonedaController extends Controller
 {
@@ -12,6 +15,11 @@ class MonedaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->response = new ResponseController();
+    }
+    
     public function index()
     {
         $data = Moneda::all();
@@ -28,9 +36,10 @@ class MonedaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+
+
     }
 
     /**
@@ -39,9 +48,38 @@ class MonedaController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(RequestMoneda $request)
     {
-        //
+        $nombre = $request->input("moneda") ?? '';
+        $simbolo = $request->input("simbolo") ?? '';
+        $codigo = $request->input("codigo") ?? '';
+        $digitos = $request->input("digitos") ?? 2;
+        $decimales = $request->input("decimales") ?? '';
+        $miles = $request->input("miles") ?? '';
+        $principal = $request->input("principal") ?? 0;
+
+
+        if($principal == 1){
+            Moneda::where('principal', '1')->update(['principal' => '0']);
+        }
+
+        $moneda = new Moneda();
+
+        $moneda->moneda = $nombre;
+        $moneda->simbolo = $simbolo;
+        $moneda->codigo = $codigo;
+        $moneda->digitos = $digitos;
+        $moneda->sep_decimales = $decimales;
+        $moneda->sep_miles = $miles;
+        $moneda->principal = $principal;
+
+        $moneda->save();
+
+        return response()->json([
+
+            'data' => $moneda
+
+        ], 200);
     }
 
     /**
@@ -50,9 +88,16 @@ class MonedaController extends Controller
      * @param  \App\Models\Moneda  $moneda
      * @return \Illuminate\Http\Response
      */
-    public function show(Moneda $moneda)
+    public function show($id)
     {
-        //
+        $moneda = Moneda::find($id);
+
+        if($moneda){
+            
+            return $this->response->success($moneda, "El registro fue encontrado");
+        }else{
+            return $this->response->error("El registro no fue encontrado");
+        }
     }
 
     /**
@@ -73,9 +118,37 @@ class MonedaController extends Controller
      * @param  \App\Models\Moneda  $moneda
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Moneda $moneda)
+    public function update(RequestMoneda $request, $id)
     {
-        //
+        $nombre = $request->input("moneda") ?? '';
+        $simbolo = $request->input("simbolo") ?? '';
+        $codigo = $request->input("codigo") ?? '';
+        $digitos = $request->input("digitos") ?? 2;
+        $decimales = $request->input("decimales") ?? '';
+        $miles = $request->input("miles") ?? '';
+        $principal = $request->input("principal") ?? 0;
+
+        if($principal == 1){
+            Moneda::where('principal', '1')->update(['principal' => '0']);
+        }
+
+        $moneda = Moneda::find($id);
+
+        $moneda->moneda = $nombre;
+        $moneda->simbolo = $simbolo;
+        $moneda->codigo = $codigo;
+        $moneda->digitos = $digitos;
+        $moneda->sep_decimales = $decimales;
+        $moneda->sep_miles = $miles;
+        $moneda->principal = $principal;
+
+        $moneda->save();
+
+        return response()->json([
+
+            'data' => $moneda
+
+        ], 200);
     }
 
     /**
@@ -84,9 +157,22 @@ class MonedaController extends Controller
      * @param  \App\Models\Moneda  $moneda
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Moneda $moneda)
+    public function destroy($id)
     {
-        //
+        $moneda = Moneda::find($id);
+
+        if(empty($moneda)){
+            return $this->response->error("No se envio un id valido");
+        }
+
+        $auth_tipo = Auth::user()->usuario_tipo;
+        if($auth_tipo !=  1 && $auth_tipo != 2){
+            return $this->response->error("El usuario no esta autorizado para realizar esta accion");
+        }
+
+        $moneda->delete();
+
+        return $this->response->success($moneda, "El registro fue eliminado correctamente");
     }
 
     public function getMonedaPrincipal()
