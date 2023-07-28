a6e73308cad76d26d82981bad81b2ac0d3a0913c
FreddyMartinezBabilonia <fredy.martinez@babilonia.io>
2023-07-28 00:19:24 -0500

    feat: creacion de controlador marca para crud

--- app/Http/Controllers/MarcasController.php
+++ app/Http/Controllers/MarcasController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Marcas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ResponseController;
+use App\Http\Requests\Marcas as MarcasRequest;
 
 class MarcasController extends Controller
 {
@@ -12,6 +15,11 @@ class MarcasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->response = new ResponseController();
+    }
+    
     public function index()
     {
         $data = Marcas::all();
@@ -39,9 +47,21 @@ class MarcasController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(MarcasRequest $request)
     {
-        //
+        $nombre = $request->input("nombre") ?? '';
+
+        $marca = new Marcas();
+
+        $marca->nombre = $nombre;
+
+        $marca->save();
+
+        return response()->json([
+
+            'data' => $marca
+
+        ], 200);
     }
 
     /**
@@ -50,9 +70,16 @@ class MarcasController extends Controller
      * @param  \App\Models\Marcas  $marcas
      * @return \Illuminate\Http\Response
      */
-    public function show(Marcas $marcas)
+    public function show($id)
     {
-        //
+        $marca = Marcas::find($id);
+
+        if($marca){
+            
+            return $this->response->success($marca, "El registro fue encontrado");
+        }else{
+            return $this->response->error("El registro no fue encontrado");
+        }
     }
 
     /**
@@ -73,9 +100,21 @@ class MarcasController extends Controller
      * @param  \App\Models\Marcas  $marcas
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marcas $marcas)
+    public function update(MarcasRequest $request, $id)
     {
-        //
+        $nombre = $request->input("nombre") ?? '';
+        
+        $marca = Marcas::find($id);
+
+        $marca->nombre = $nombre;
+
+        $marca->save();
+
+        return response()->json([
+
+            'data' => $marca
+
+        ], 200);
     }
 
     /**
@@ -84,8 +123,21 @@ class MarcasController extends Controller
      * @param  \App\Models\Marcas  $marcas
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marcas $marcas)
+    public function destroy($id)
     {
-        //
+        $marcas = Marcas::find($id);
+
+        if(empty($marcas)){
+            return $this->response->error("No se envio un id valido");
+        }
+
+        $auth_tipo = Auth::user()->usuario_tipo;
+        if($auth_tipo !=  1 && $auth_tipo != 2){
+            return $this->response->error("El usuario no esta autorizado para realizar esta accion");
+        }
+
+        $marcas->delete();
+
+        return $this->response->success($marcas, "El registro fue eliminado correctamente");
     }
 }
---
+++ app/Http/Requests/Marcas.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class Marcas extends FormRequest
+{
+    public function rules(Request $request)
+    {
+        return [
+            'nombre' => 'required',
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+
+    }
+}
