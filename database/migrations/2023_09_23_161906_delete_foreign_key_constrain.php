91c79e9359326fa66e576a8bc83d8959cc78cb6b
Fredy <fmartinez.bpe@gmail.com>
2023-09-23 11:47:18 -0500

    fix: se elimino foreign_key_constrain en stock_helados_detail stock_helados_detail_codigo_foreign

---
+++ database/migrations/2023_09_23_161906_delete_foreign_key_constrain.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stock_helados_detail', function (Blueprint $table) {
+            $table->dropForeign('stock_helados_detail_codigo_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            //
+        });
+    }
+};
