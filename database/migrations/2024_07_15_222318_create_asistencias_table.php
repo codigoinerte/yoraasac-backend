d11319519f2b654db4175e8619b799dd421da220
codigoinerte <fmartinez.bpe@gmail.com>
2024-07-20 14:48:15 -0500

    feat: se agrego nota_id en table de asistencias

--- database/migrations/2024_07_15_222318_create_asistencias_table.php
+++ database/migrations/2024_07_15_222318_create_asistencias_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('asistencias', function (Blueprint $table) {
             $table->id();
             $table->integer("user_id");
+            $table->integer("nota_id");
             $table->timestamp('fecha')->useCurrent();
             $table->timestamps();
         });
